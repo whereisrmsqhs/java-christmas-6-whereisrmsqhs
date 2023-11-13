a52f9d187225b525cf2d5c4e87a92eebfdd87191
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-13 16:12:35 +0900

    feat(DateOfTheWeek): 다음 요일로 넘어가는 기능 구현

--- src/main/java/christmas/domain/DateOfTheWeek.java
+++ src/main/java/christmas/domain/DateOfTheWeek.java
@@ -2,27 +2,46 @@ package christmas.domain;
 
 import christmas.constant.WeekDaysAndWeekEnds;
 
+import java.util.Arrays;
+
 import static christmas.constant.WeekDaysAndWeekEnds.WEEK_DAYS;
 import static christmas.constant.WeekDaysAndWeekEnds.WEEK_ENDS;
 
 public enum DateOfTheWeek {
-    MONDAY("월료일", WEEK_DAYS),
-    TUESDAY("화요일", WEEK_DAYS),
-    WEDNESDAY("수요일", WEEK_DAYS),
-    THURSDAY("목요일", WEEK_DAYS),
-    FRIDAY("금요일", WEEK_ENDS),
-    SATURDAY("토요일", WEEK_ENDS),
-    SUNDAY("일요일", WEEK_DAYS);
+    MONDAY(1, "월료일", WEEK_DAYS),
+    TUESDAY(2, "화요일", WEEK_DAYS),
+    WEDNESDAY(3, "수요일", WEEK_DAYS),
+    THURSDAY(4, "목요일", WEEK_DAYS),
+    FRIDAY(5, "금요일", WEEK_ENDS),
+    SATURDAY(6, "토요일", WEEK_ENDS),
+    SUNDAY(7, "일요일", WEEK_DAYS);
 
+    private final Integer index;
     private final String dayOfTheWeek;
     private final WeekDaysAndWeekEnds weekdayType;
 
-    DateOfTheWeek(String dayOfTheWeek, WeekDaysAndWeekEnds weekdayType) {
+    DateOfTheWeek(Integer index, String dayOfTheWeek, WeekDaysAndWeekEnds weekdayType) {
+        this.index = index;
         this.dayOfTheWeek = dayOfTheWeek;
         this.weekdayType = weekdayType;
     }
 
+    public Integer getIndex() {
+        return index;
+    }
+
     public boolean isSpecialDay(Integer day) {
         return (this.dayOfTheWeek.equals("일요일") || day == 25);
+    }
+
+    public DateOfTheWeek calculateNextDateOfTheWeek() {
+        int tempNextIndex = index + 1;
+        if(index + 1 == 8) tempNextIndex = 1;
+        Integer nextIndex = tempNextIndex;
+
+        return Arrays.stream(DateOfTheWeek.values())
+                .filter(date -> date.getIndex().equals(nextIndex))
+                .findFirst()
+                .orElse(null);
     }
 }
