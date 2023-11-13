83f618b944ae0b4e75c729bae30b0d40fc22443f
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-13 18:01:18 +0900

    refactor(Menu, Badge): Price객체를 적용한 필드로 수정

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -1,2 +1,19 @@
-package christmas.domain;public class Badge {
+package christmas.domain;
+
+public enum Badge {
+    STAR("별", new Price(5000)),
+    TREE("트리", new Price(10000)),
+    SANTA("산타", new Price(20000));
+
+    private String badgeName;
+    private Price acquiredCondition;
+
+    Badge(String badgeName, Price acquiredCondition) {
+        this.badgeName = badgeName;
+        this.acquiredCondition = acquiredCondition;
+    }
+
+    public String getBadgeName() {
+        return badgeName;
+    }
 }
--- src/main/java/christmas/domain/Menu.java
+++ src/main/java/christmas/domain/Menu.java
@@ -7,24 +7,24 @@ import java.util.Arrays;
 import static christmas.constant.MenuCategory.*;
 
 public enum Menu {
-    MUSHROOM_SOUP("양송이수프", 6000, APPETIZER),
-    TAPAS("양송이수프", 5500, APPETIZER),
-    CAESAR_SALAD("양송이수프", 8000, APPETIZER),
-    T_BONE_STEAK("티본스테이크", 55000, MAIN),
-    BBQ_RIBS("바비큐립", 54000, MAIN),
-    SEAFOOD_PASTA("해산물파스타", 35000, MAIN),
-    CHRISTMAS_PASTA("크리스마스파스타", 25000, MAIN),
-    CHOCOLATE_CAKE("초코케이크", 15000, DESSERT),
-    ICE_CREAM("아이스크림", 5000, DESSERT),
-    ZERO_COKE("제로콜라", 3000, DRINK),
-    RED_WINE("레드와인", 60000, DRINK),
-    CHAMPAGNE("샴페인", 25000, DRINK);
+    MUSHROOM_SOUP("양송이수프", new Price(6000), APPETIZER),
+    TAPAS("양송이수프", new Price(5500), APPETIZER),
+    CAESAR_SALAD("양송이수프", new Price(8000), APPETIZER),
+    T_BONE_STEAK("티본스테이크", new Price(55000), MAIN),
+    BBQ_RIBS("바비큐립", new Price(54000), MAIN),
+    SEAFOOD_PASTA("해산물파스타", new Price(35000), MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", new Price(25000), MAIN),
+    CHOCOLATE_CAKE("초코케이크", new Price(15000), DESSERT),
+    ICE_CREAM("아이스크림", new Price(5000), DESSERT),
+    ZERO_COKE("제로콜라", new Price(3000), DRINK),
+    RED_WINE("레드와인", new Price(60000), DRINK),
+    CHAMPAGNE("샴페인", new Price(25000), DRINK);
 
     private final String name;
-    private final Integer price;
+    private final Price price;
     private final MenuCategory type;
 
-    Menu(String name, Integer price, MenuCategory type) {
+    Menu(String name, Price price, MenuCategory type) {
         this.name = name;
         this.price = price;
         this.type = type;
