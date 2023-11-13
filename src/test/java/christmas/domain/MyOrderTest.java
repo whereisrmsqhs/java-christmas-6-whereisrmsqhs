5ceaa55618b5d29eb5d70eedce7d214ffaf92789
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-13 22:16:07 +0900

    test(MyOrderTest): 할인전 총 주문 금액 계산 테스트

--- src/test/java/christmas/domain/MyOrderTest.java
+++ src/test/java/christmas/domain/MyOrderTest.java
@@ -1,4 +1,23 @@
+package christmas.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MyOrderTest {
-  
+
+    @ParameterizedTest
+    @CsvSource(value = {"티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:142,000"}, delimiter = ':')
+    @DisplayName("주문한 메뉴 정보가 주어졌을 때 할인전 총 금액을 계산하는 기능")
+    void calculateTotalAmountBeforeDiscount(String textOrder, String expectedResult) {
+        MyOrder myOrder = new MyOrder(textOrder);
+        Price totalAmountBeforeDiscount = myOrder.calculateTotalAmountBeforeDiscount();
+
+        Assertions.assertThat(totalAmountBeforeDiscount.convertToOutputType()).isEqualTo(expectedResult);
+
+    }
+
 }
