84a6a11c5360b088f44d1898937600fb46e20572
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 01:39:58 +0900

    feat(PlannerService): 증정 이벤트 받을 수 있는지 판단하는 기능 구현

--- src/main/java/christmas/service/PlannerService.java
+++ src/main/java/christmas/service/PlannerService.java
@@ -1,2 +1,32 @@
-package christmas.service;public class PlannerService {
+package christmas.service;
+
+import christmas.domain.*;
+
+public class PlannerService {
+    public Price calculateDDayDiscount(MyRequirement requirement, December december) {
+        ChristmasDiscountPolicy policy = new ChristmasDiscountPolicy();
+        return requirement.calculateDiscountByPolicy(policy, december);
+    }
+
+    public Price calculateWeekDayDiscount(MyRequirement requirement, December december) {
+        WeekDayDiscountPolicy policy = new WeekDayDiscountPolicy();
+        return requirement.calculateDiscountByPolicy(policy, december);
+    }
+
+    public Price calculateWeekendDiscount(MyRequirement requirement, December december) {
+        WeekEndDiscountPolicy policy = new WeekEndDiscountPolicy();
+        return requirement.calculateDiscountByPolicy(policy, december);
+    }
+
+    public Price calculateSpecialDiscount(MyRequirement requirement, December december) {
+        SpecialDiscountPolicy policy = new SpecialDiscountPolicy();
+        return requirement.calculateDiscountByPolicy(policy, december);
+    }
+
+    public Price includeChampaigneGift(Price totalAmountBeforeDiscount) {
+        if(totalAmountBeforeDiscount.isHigherThanPresentationThreshold()) {
+            return Menu.getChamPaginePrice();
+        }
+        return new Price(0);
+    }
 }
