fce152e6c733a42c0a9e27395c9f4c8a7c0386fa
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 01:13:01 +0900

    feat(SpecialDiscountPolicy): 특별할인 정책 구현 코드

---
+++ src/main/java/christmas/domain/SpecialDiscountPolicy.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+import static christmas.constant.NumbersOrSymbols.SPECIAL_DISCOUNT_AMOUNT;
+
+public class SpecialDiscountPolicy implements DiscountPolicy{
+
+    @Override
+    public Price discount(MyOrder myOrder, Integer visitDate, December december) {
+        Date lookedForDate = december.findSpecificDate(visitDate);
+        if (lookedForDate.isSpecial()) {
+            return new Price(SPECIAL_DISCOUNT_AMOUNT);
+        }
+        return new Price(0);
+    }
+}
