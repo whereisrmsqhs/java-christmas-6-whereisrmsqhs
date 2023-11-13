c6046a4fbb8207c926b43bb96c1f577409666309
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 00:48:22 +0900

    feat(WeekDayDiscountPolicy): 평일 총 할인 금액 계산 구현

--- src/main/java/christmas/domain/WeekDayDiscountPolicy.java
+++ src/main/java/christmas/domain/WeekDayDiscountPolicy.java
@@ -1,2 +1,12 @@
-package christmas.domain;public class WeekDayDiscountPolicy {
+package christmas.domain;
+
+public class WeekDayDiscountPolicy implements DiscountPolicy{
+    @Override
+    public Price discount(MyOrder myOrder, Integer visitDate, December december) {
+        Date lookedForDate = december.findSpecificDate(visitDate);
+        if(lookedForDate.isWeekDay()) {
+            return myOrder.calculateDiscountInWeekDay();
+        }
+        return new Price(0);
+    }
 }
