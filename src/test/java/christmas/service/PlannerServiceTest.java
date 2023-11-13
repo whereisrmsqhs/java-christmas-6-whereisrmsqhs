44f3a167a5e949124d09779e2bb1f5ef25fb265f
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 01:40:55 +0900

    test(PlannerServiceTest): 총 할인전 구매 금액이 12만원 이상이면 샴페인 증정 기능 테스트

--- src/test/java/christmas/service/PlannerServiceTest.java
+++ src/test/java/christmas/service/PlannerServiceTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package christmas.service;
+
+import christmas.domain.Price;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+
 class PlannerServiceTest {
-  
+
+    @ParameterizedTest
+    @CsvSource(value = {"1000:0", "120000:25,000"}, delimiter = ':')
+    @DisplayName("할인전 총 주문 가격이 12만원 이상이면 샴페인 가격을 리턴")
+    void calculatePresentationAvailable(Integer inputPrice, String expected) {
+        PlannerService service = new PlannerService();
+        Price resultPrice = service.includeChampaigneGift(new Price(inputPrice));
+
+        assertThat(resultPrice.convertToOutputType()).isEqualTo(expected);
+    }
+
 }
