e26627403a5486d856081b5c4a91f56a05f10b3a
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-13 23:50:18 +0900

    test(ChristmasDiscountPolicyTest): 크리스마스 디데이 할인 적용 금액 테스트

--- src/test/java/christmas/domain/ChristmasDiscountPolicyTest.java
+++ src/test/java/christmas/domain/ChristmasDiscountPolicyTest.java
@@ -1,4 +1,24 @@
+package christmas.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ChristmasDiscountPolicyTest {
-  
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:1,000", "25:3,400", "26:0"}, delimiter = ':')
+    @DisplayName("날짜 Integer가 주어졌을 때 해당 날짜 크리스마스 할인 금액 계산")
+    void calculateChristmasDiscount(Integer date, String expected) {
+        December december = December.getInstance();
+        ChristmasDiscountPolicy policy = new ChristmasDiscountPolicy();
+
+        Price discount = policy.discount(date, december);
+
+        Assertions.assertThat(discount.convertToOutputType()).isEqualTo(expected);
+    }
+
 }
