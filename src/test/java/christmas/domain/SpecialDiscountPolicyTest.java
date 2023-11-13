8edb51febaf2d654a118896cdf953b7f6e974ee9
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 01:13:46 +0900

    test(SpecialDiscountPolicyTest): 특별날에 적용되는 경우 1000원 할인해주는 메소드 테스트

--- src/test/java/christmas/domain/SpecialDiscountPolicyTest.java
+++ src/test/java/christmas/domain/SpecialDiscountPolicyTest.java
@@ -1,4 +1,32 @@
+package christmas.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SpecialDiscountPolicyTest {
-  
+
+    @ParameterizedTest
+    @MethodSource("generateMyOrderVisitDateAndExpectResult")
+    @DisplayName("예약한 날짜가 특별한 날이면 특별할인이 들어간다.")
+    void calculateSpecialDiscount(MyOrder myOrder, Integer visitDate, Price expectedResult) {
+        SpecialDiscountPolicy policy = new SpecialDiscountPolicy();
+        Price discountPrice = policy.discount(myOrder, visitDate, December.getInstance());
+
+        Assertions.assertThat(discountPrice.convertToOutputType()).isEqualTo(expectedResult.convertToOutputType());
+    }
+
+    static Stream<Arguments> generateMyOrderVisitDateAndExpectResult() {
+        return Stream.of(
+                Arguments.of(new MyOrder("초코케이크-2"), 3, new Price(1000)),
+                Arguments.of(new MyOrder("초코케이크-2"), 1, new Price(0))
+        );
+    }
+
 }
