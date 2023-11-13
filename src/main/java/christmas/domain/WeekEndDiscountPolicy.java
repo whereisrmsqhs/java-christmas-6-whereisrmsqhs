9fe891649431490e916742090f4e483ed11c9e31
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 01:03:06 +0900

    feat(WeekEndDiscountPolicy): 주말 총 할인 금액 계산 구현

---
+++ src/main/java/christmas/domain/WeekEndDiscountPolicy.java
@@ -0,0 +1,12 @@
+package christmas.domain;
+
+public class WeekEndDiscountPolicy implements DiscountPolicy{
+    @Override
+    public Price discount(MyOrder myOrder, Integer visitDate, December december) {
+        Date lookedForDate = december.findSpecificDate(visitDate);
+        if(lookedForDate.isWeekEnd()) {
+            return myOrder.calculateDiscountInWeekEnd();
+        }
+        return new Price(0);
+    }
+}
