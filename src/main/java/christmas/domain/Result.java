8e7b848259b1611c2413f277c32ef4a6177f2995
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 20:23:37 +0900

    feat(Result): 할인하는 이벤트를 출력 형식으로 변경해주는 기능 추가

--- src/main/java/christmas/domain/Result.java
+++ src/main/java/christmas/domain/Result.java
@@ -1,2 +1,29 @@
-package christmas.domain;public record Result() {
+package christmas.domain;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record Result(Price christmasEventDiscountAmount, Price weekDayDiscountAmount, Price weekEndDiscountAmount,
+                     Price specialDiscountAmount, Price champaigneAmount) {
+    public boolean receiveChampaigneGift() {
+        return champaigneAmount.getPrice() != 0;
+    }
+
+    private List<Price> getAllFields() {
+        List<Price> allDiscountAmounts = new LinkedList<>();
+        allDiscountAmounts.add(christmasEventDiscountAmount);
+        allDiscountAmounts.add(weekDayDiscountAmount);
+        allDiscountAmounts.add(weekEndDiscountAmount);
+        allDiscountAmounts.add(specialDiscountAmount);
+        allDiscountAmounts.add(champaigneAmount);
+        return allDiscountAmounts;
+    }
+
+    public List<String> bringDiscountDetails() {
+        List<Price> priceList = getAllFields();
+        return priceList.stream()
+                .map(Price::convertToOutputType)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
 }
