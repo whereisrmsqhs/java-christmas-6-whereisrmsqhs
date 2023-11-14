ccc389ca613acea45cd3412187ea38af0d01a396
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-15 00:05:10 +0900

    feat(MyOrderValidation): 메뉴 주문시 검증 기능 구현

--- src/main/java/christmas/validation/MyOrderValidation.java
+++ src/main/java/christmas/validation/MyOrderValidation.java
@@ -1,2 +1,29 @@
-package christmas.validation;public class MyOrderValidation {
+package christmas.validation;
+
+import christmas.domain.Menu;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static christmas.constant.ErrorMessage.ERROR;
+import static christmas.constant.ErrorMessage.INVALID_INPUT;
+
+public class MyOrderValidation {
+    public static void validateOrderInfo(String myOrderedFoods) {
+        String pattern = "([가-힣]+-\\d+)(,([가-힣]+-\\d+))*";
+        boolean isMatch = Pattern.matches(pattern, myOrderedFoods);
+        if (isMatch == false)
+            throw new IllegalArgumentException(ERROR + INVALID_INPUT);
+    }
+
+    public static void validateFood(Menu myOrderedFood, Map<Menu, Integer> myAllOrderedMenus) {
+        if (myOrderedFood.equals(null)) throw new IllegalArgumentException(ERROR + INVALID_INPUT);
+        if (!myAllOrderedMenus.get(myOrderedFood).equals(null))
+            throw new IllegalArgumentException(ERROR + INVALID_INPUT);
+    }
+
+    public static void validateOrderNumber(Integer myOrderFoodNumber) {
+        if (myOrderFoodNumber < 1)
+            throw new IllegalArgumentException(ERROR + INVALID_INPUT);
+    }
 }
