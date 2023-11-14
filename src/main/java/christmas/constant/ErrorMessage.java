fb88a8ed4fbab520e47c9d0db6e84a544756599e
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-15 01:40:47 +0900

    feat(ErrorMessage): 예외 발생시 출력하는 에러 메시지 정리

--- src/main/java/christmas/constant/ErrorMessage.java
+++ src/main/java/christmas/constant/ErrorMessage.java
@@ -1,2 +1,8 @@
-package christmas.constant;public class ErrorMessage {
+package christmas.constant;
+
+public class ErrorMessage {
+    public static final String ERROR = "[ERROR]";
+    public static final String INVALID_INPUT = " 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String ONLY_DRINKS = " 음료만 주문 시, 주문할 수 없습니다.";
+
 }
