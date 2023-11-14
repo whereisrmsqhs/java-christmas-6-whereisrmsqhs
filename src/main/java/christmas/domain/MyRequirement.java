9774a4656059c5d5ec529a9175a48cca991c00cb
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 20:21:37 +0900

    feat(MyRequirement): 할인전 가격과 할인을 받을 수 있는 가격 계산하는 기능 추가

--- src/main/java/christmas/domain/MyRequirement.java
+++ src/main/java/christmas/domain/MyRequirement.java
@@ -1,2 +1,12 @@
-package christmas.domain;public class MyRequirement {
+package christmas.domain;
+
+public record MyRequirement(MyOrder myOrder, Integer visitDate) {
+
+    public Price calculateTotalAmountBeforeDiscount() {
+        return myOrder.calculateTotalAmountBeforeDiscount();
+    }
+
+    public Price calculateDiscountByPolicy(DiscountPolicy discountPolicy, December december) {
+        return discountPolicy.discount(myOrder, visitDate, december);
+    }
 }
