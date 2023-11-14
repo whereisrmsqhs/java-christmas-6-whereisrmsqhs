1a4872fdebee20d7d2abfeb6e58d20f995528cd4
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 20:13:56 +0900

    feat(constant): 프로젝트에서 사용하는 상수 값 모음

--- src/main/java/christmas/constant/NumbersOrSymbols.java
+++ src/main/java/christmas/constant/NumbersOrSymbols.java
@@ -1,2 +1,15 @@
-package christmas.constant;public class NumbersOrSymbols {
+package christmas.constant;
+
+public class NumbersOrSymbols {
+    public static String DIVIDE_EACH_ORDERED_MENU = ",";
+    public static String COLON = ":";
+    public static String DIVIDE_FOOD_NUMBER = "-";
+    public static String UNIT = "개";
+    public static Integer MAX_CHRISTMAS_D_DAY_DISCOUNT = 3400;
+    public static Integer EACH_DAY_DISCOUNT_UNIT = 100;
+    public static Integer WEEKDAY_WEEKEND_EVENT_PER_DISCOUNT = 2023;
+    public static Integer SPECIAL_DISCOUNT_AMOUNT = 1000;
+    public static Integer PRESERNTATION_THRESHOLD = 120000;
+    public static String GIFT_PRESENTED = "샴페인 1개";
+    public static String NO_GIFT = "없음";
 }
--- src/main/java/christmas/constant/OutputviewMessages.java
+++ src/main/java/christmas/constant/OutputviewMessages.java
@@ -1,2 +1,26 @@
-package christmas.constant;public class OutputviewMessages {
+package christmas.constant;
+
+public class OutputviewMessages {
+    public static final String FIRST_GREETING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static final String ASK_VISITING_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String ORDER_MENU_GUIDE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String PREVIEW_EVENT = "에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    public static final String MONTH = "12월 ";
+    public static final String DAY_UNIT = "일";
+    public static final String ORDERED_MENU = "<주문 메뉴>";
+    public static final String TOTAL_ORDER_AMOUNT_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
+    public static final String GIFT_MENU = "<증정 메뉴>";
+    public static final String BENEFITS_DETAIL = "<혜택 내역>";
+    public static final String TOTAL_BENEFIT_DETAIL = "<총혜택 금액>";
+    public static final String EXPECTED_AMOUNT_OF_PAYMENT_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
+    public static final String EVENT_BADGE = "<12월 이벤트 배지>";
+    public static final String WON_UNIT = "원";
+    public static final String CHRISTMAS_DDAY_DISCOUNT = "크리스마스 디데이 할인: ";
+    public static final String WEEKDAY_DISCOUNT = "평일 할인: ";
+    public static final String WEEKEND_DISCOUNT = "주말 할인: ";
+    public static final String SPECIAL_DISCOUNT = "특별 할인: ";
+    public static final String GIFT_EVENT_DISCOUNT = "증정 이벤트: ";
+    public static final String MINUS = "-";
+    public static final String ENTER = "\n";
+
 }
