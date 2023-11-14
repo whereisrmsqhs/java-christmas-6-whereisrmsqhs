f13076ba3eccf5ea32d0b9cb5a96789d30ced28e
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-15 00:06:17 +0900

    test(MyOrderValidation): 메뉴 주문 입력 형식 검증 테스트

--- src/test/java/christmas/validation/MyOrderValidationTest.java
+++ src/test/java/christmas/validation/MyOrderValidationTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package christmas.validation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class MyOrderValidationTest {
-  
+
+    @ParameterizedTest
+    @ValueSource(strings = {"여기 해산물파스타 하나요~", "해산물파스타-하나"})
+    @DisplayName("음식 주문 입력값은 특정한 형태를 유지 하면 안되면 에러를 리턴한다.")
+    void validateFoodOrderFormat(String input) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            MyOrderValidation.validateOrderInfo(input);
+        });
+    }
 }
