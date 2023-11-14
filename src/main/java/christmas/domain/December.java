81adf1ffb82afaeff29132371eaa51d5989bf849
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 20:18:38 +0900

    feat(December): 단순 정수로 특정날짜를 주었을 때 12월의 어떤 날에 해당되는지 요일등 특성과 함께 꺼내는 기능 추가

--- src/main/java/christmas/domain/December.java
+++ src/main/java/christmas/domain/December.java
@@ -1,2 +1,32 @@
-package christmas.domain;public class December {
+package christmas.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static christmas.domain.DateOfTheWeek.FRIDAY;
+
+public class December {
+    private static December instance;
+    private static final List<Date> allDates = new ArrayList<>();
+
+    private December() {
+        create31Days(FRIDAY);
+    }
+
+    public static December getInstance() {
+        instance = new December();
+        return instance;
+    }
+
+    private void create31Days(DateOfTheWeek dateOfTheWeek) {
+        DateOfTheWeek currentDateOfTheWeek = dateOfTheWeek;
+        for (int i = 1; i <= 31; i++) {
+            allDates.add(new Date(currentDateOfTheWeek, i));
+            currentDateOfTheWeek = currentDateOfTheWeek.calculateNextDateOfTheWeek();
+        }
+    }
+
+    public Date findSpecificDate(Integer visitDate) {
+        return allDates.get(visitDate-1);
+    }
 }
