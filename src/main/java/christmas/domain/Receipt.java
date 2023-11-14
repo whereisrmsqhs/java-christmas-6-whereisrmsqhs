2057f1725c49b25b9804b85fbab0eca2351043f3
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-14 20:22:49 +0900

    feat(Receipt): 각 혜택 상세 내용들을 반환하는 각 기능을 구현

--- src/main/java/christmas/domain/Receipt.java
+++ src/main/java/christmas/domain/Receipt.java
@@ -1,2 +1,61 @@
-package christmas.domain;public class Receipt {
+package christmas.domain;
+
+import java.util.List;
+
+import static christmas.constant.NumbersOrSymbols.GIFT_PRESENTED;
+import static christmas.constant.NumbersOrSymbols.NO_GIFT;
+
+public class Receipt {
+
+    private final MyRequirement requirement;
+    private final Price totalAmountBeforeDiscount;
+    private final Result eachDiscountAmount;
+    private final Price totalDiscountAmount;
+    private final Badge eventBadge;
+
+    public Receipt(MyRequirement requirement, Price totalAmountBeforeDiscount,
+                   Result eachDiscountAmount, Price totalDiscountAmount, Badge eventBadge) {
+        this.requirement = requirement;
+
+        this.totalAmountBeforeDiscount = totalAmountBeforeDiscount;
+        this.eachDiscountAmount = eachDiscountAmount;
+        this.totalDiscountAmount = totalDiscountAmount;
+        this.eventBadge = eventBadge;
+    }
+
+    public int visitDate() {
+        return requirement.visitDate();
+    }
+
+    public MyOrder getMyOrder() {
+        return requirement.myOrder();
+    }
+
+    public String printingTotalAmountBeforeDiscount() {
+        return totalAmountBeforeDiscount.convertToOutputType();
+    }
+
+    public String isThereChampagineGift() {
+        if (eachDiscountAmount.receiveChampaigneGift()) {
+            return GIFT_PRESENTED;
+        }
+        return NO_GIFT;
+    }
+
+    public List<String> bringEachDiscountInfo() {
+        return eachDiscountAmount.bringDiscountDetails();
+    }
+
+    public String getTotalDiscountAmount() {
+        return totalDiscountAmount.convertToOutputType();
+    }
+
+    public String getExpectedPayment() {
+        Price totalAmountIncludeGift = totalAmountBeforeDiscount.difference(totalDiscountAmount);
+        return totalAmountIncludeGift.add(eachDiscountAmount.champaigneAmount());
+    }
+
+    public String getEventBadge() {
+        return eventBadge.getBadgeName();
+    }
 }
